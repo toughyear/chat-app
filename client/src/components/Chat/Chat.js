a37de5e10f6bed17364a88ca392f67db54d88331
Rajeev Singh Naruka <rajivsingh430@gmail.com>
2020-01-22 15:12:36 +0530

    App is now working

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,30 +2,76 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
-//empty variable
+import TextContainer from "../TextContainer/TextContainer";
+import Messages from "../Messages/Messages";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+
+import "./Chat.css";
+
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = "localhost:5000";
 
   useEffect(() => {
-    //called once when component mounts
     const { name, room } = queryString.parse(location.search);
 
-    //create a socket for client
     socket = io(ENDPOINT);
 
-    setName(name);
     setRoom(room);
+    setName(name);
 
-    //create custom events with client side socket
-
-    socket.emit("join", { name, room }, () => {});
+    socket.emit("join", { name, room }, error => {
+      if (error) {
+        alert(error);
+      }
+    });
   }, [ENDPOINT, location.search]);
 
-  return <h1>CHAT</h1>;
+  useEffect(() => {
+    socket.on("message", message => {
+      setMessages([...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+
+    return () => {
+      socket.emit("disconnect");
+
+      socket.off();
+    };
+  }, [messages]);
+
+  const sendMessage = event => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
 };
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,36 +1,67 @@
-//requirements
+const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-const http = require("http");
-const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const router = require("./router");
-// create a app that has express functionalities
+
 const app = express();
-
-//The http.createServer() method creates an HTTP Server object.
 const server = http.createServer(app);
-
-// socket for our app to communicate via the server
 const io = socketio(server);
 
-// io will now handle all connections between serverside of app and client side of app.
-// here the io is server side. It has some built-in methods like connection and disconnect.abs
+app.use(cors());
+app.use(router);
 
-io.on("connection", socket => {
-  console.log(`A new user has connected with ID = ${socket.id.substr(0, 5)}`);
-
+io.on("connect", socket => {
   socket.on("join", ({ name, room }, callback) => {
-    console.log("User:", name, "Room:", room);
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-    //call back function ... you can name it anything
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to room ${user.room}.`
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    });
+
+    callback();
   });
-  // now connection has established between client-socket and server.
-  // we can call special methods on the client socket we just connected to.
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
   socket.on("disconnect", () => {
-    console.log("user left!");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+    }
   });
 });
-app.use(router);
-//start server
-server.listen(PORT, () => console.log(`server has started on port: ${PORT}`));
+
+server.listen(process.env.PORT || 5000, () =>
+  console.log(`Server has started.`)
+);
